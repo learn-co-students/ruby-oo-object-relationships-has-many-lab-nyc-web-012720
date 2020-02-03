7cb39e2ebc1dc64e23c2eef55e66e2f6375586da
github username <github email address>
2020-02-03 13:42:42 -0500

    Done.

---
+++ lib/run.rb
@@ -0,0 +1,29 @@
+# require_relative './artist'
+# require_relative './song'
+# require 'pry'
+# # require_relative './artist'
+# # require_relative './artist'
+
+# eminem = Artist.new("Eminem")
+# jay_z = Artist.new("JAY-Z")
+# drake = Artist.new("Drake")
+
+# my_name = Song.new("My Name Is")
+# slim_shady = Song.new("The Real Slim Shady")
+# zero_to_one_hundred = Song.new("0 To 100")
+# back_to_back = Song.new("Back 2 Back")
+# run_this_town = Song.new("Run This Town")
+# dead_presidents = Song.new("Dead Presidents")
+# venus_mars = Song.new("Venus vs Mars")
+
+# my_name.artist = eminem
+# slim_shady.artist = eminem
+# zero_to_one_hundred.artist = drake
+# back_to_back.artist = drake
+# run_this_town.artist = jay_z
+# dead_presidents.artist = jay_z
+# venus_mars.artist = jay_z
+
+# drake.song = zero_to_one_hundred
+# drake.add_song
+# binding.pry
